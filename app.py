73070e03e3fa68925264e12e5aa4a88bc042addf
jjjjjjjjhhh <eejh-wp21@student.tarc.edu.my>
2023-09-20 00:08:46 +0800

    s

--- app.py
+++ app.py
@@ -8,12 +8,59 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
 db = SQLAlchemy(app)
 
-# Define the Company model with schema 'employee'
+#routes
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+@app.route("/adminCom")
+def adminCom():
+    return render_template('adminCom.html')
+
+@app.route("/adminLec")
+def adminLec():
+    return render_template('adminLec.html')
+
+@app.route("/adminStud")
+def adminStud():
+    return render_template('adminStud.html')
+
+@app.route("/AfterSubmit")
+def AfterSubmit():
+    return render_template('AfterSubmit.html')
+
+@app.route("/jobdetail1")
+def jobdetail1():
+    return render_template('job-detail-1.html')
+
+@app.route("/jobdetail2")
+def jobdetail2():
+    return render_template('job-detail-2.html')
+
+@app.route("/jobdetail3")
+def jobdetail3():
+    return render_template('job-detail-3.html')
+
+@app.route("/jobdetail4")
+def jobdetail4():
+    return render_template('job-detail-4.html')
+
+@app.route("/jobdetail5")
+def jobdetail5():
+    return render_template('job-detail-5.html')
+
+
+# Define model
 class Company(db.Model):
     __tablename__ = 'Company'  # Table name (case-sensitive)
     __table_args__ = {'schema': 'employee'}  # Schema name
     
-    # Define your table columns here
+    
     company_id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String(255))
     industry = db.Column(db.String(255))
@@ -22,13 +69,40 @@ class Company(db.Model):
     pic_email = db.Column(db.String(255))
     pic_phone_number = db.Column(db.String(255))
 
-@app.route('/')
-def index():
-    # Retrieve Company data from the database
+class PositionTable(db.Model):
+    __tablename__ = 'PositionTable'  # Table name (case-sensitive)
+    __table_args__ = {'schema': 'employee'}  # Schema name
+    
+    
+    position_id = db.Column(db.String(5), primary_key=True)
+    company_id = db.Column(db.String(5), db.ForeignKey('Company.company_id'), nullable=False)
+    position = db.Column(db.String(50), nullable=False)
+    job_desc = db.Column(db.String(255), nullable=False)
+    allowance = db.Column(db.DECIMAL(precision=6, scale=2), nullable=False)
+
+    # Define a relationship to the Company table
+    company = db.relationship('Company', backref='positions')
+
+    def __init__(self, position_id, company_id, position, job_desc, allowance):
+        self.position_id = position_id
+        self.company_id = company_id
+        self.position = position
+        self.job_desc = job_desc
+        self.allowance = allowance
+
+@app.route('/GetComOutput')
+def GetComOutput():
+   
     companies = Company.query.all()
-    # Fetch student data from the database if needed
-    # students = Student.query.all()
+    
     return render_template('GetComOutput.html', companies=companies)
+
+@app.route('/GetPosOutput')
+def GetPosOutput():
+   
+    positions = positions.query.all()
+    
+    return render_template('GetPosOutput.html', positions=positions)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
