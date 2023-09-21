9848d98d1ec1e5f40b747b9b83a51f068044d618
jjjjjjjjhhh <eejh-wp21@student.tarc.edu.my>
2023-09-21 15:02:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ customdb = "employee"
 custombucket = "natalietanjieyin-bucket"
 customregion = "us-east-1"
 dbport = "3306"
+httpd = "54.234.186.136"
 
 DB_URI = f"mysql+pymysql://{customuser}:{custompass}@{customhost}:{dbport}/{customdb}"
 
